312e21e963f52171b78919cf687d1349116e0b5c
onemen <tabmix.onemen@gmail.com>
2026-06-02 16:01:12 +0300

    spy on fetch

--- test/registryType.test.ts
+++ test/registryType.test.ts
@@ -4,7 +4,30 @@ import ncu from '../src/index'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+const { fetch: originalFetch } = global
+const TARGET_URL =
+  'https://gist.githubusercontent.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9/raw/6f22bfdf19b7596089e56e0b14cd66d077f049d5/staticRegistry.json'
+
 describe('staticRegistry', function () {
+  beforeAll(() => {
+    vi.stubGlobal('fetch', vi.fn())
+    vi.mocked(fetch).mockImplementation(async input => {
+      if (input === TARGET_URL) {
+        return new Response(JSON.stringify({ 'ncu-test-v2': '99.9.9' }), {
+          status: 200,
+          headers: { 'Content-Type': 'application/json' },
+        })
+      }
+      // Fallback for other inputs
+      console.warn('Missing fixture for URL:', input)
+      return originalFetch(input)
+    })
+  })
+
+  afterAll(() => {
+    vi.unstubAllGlobals()
+  })
+
   it('upgrade to the version specified in the static registry file', async () => {
     const output = await ncu({
       packageData: {
@@ -43,9 +66,7 @@ describe('staticRegistry', function () {
         },
       },
       registryType: 'json',
-      registry:
-        // https://gist.github.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9
-        'https://gist.githubusercontent.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9/raw/6f22bfdf19b7596089e56e0b14cd66d077f049d5/staticRegistry.json',
+      registry: TARGET_URL, // https://gist.github.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9
     })
 
     output!.should.deep.equal({})
@@ -73,9 +94,7 @@ describe('staticRegistry', function () {
           'ncu-test-v2': '1.0.0',
         },
       },
-      registry:
-        // https://gist.github.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9
-        'https://gist.githubusercontent.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9/raw/6f22bfdf19b7596089e56e0b14cd66d077f049d5/staticRegistry.json',
+      registry: TARGET_URL, // https://gist.github.com/raineorshine/0802d7388c69193bed49c5ee6ab611b9
     })
 
     output!.should.deep.equal({
