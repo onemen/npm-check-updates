d6fd63bc8d63b4634fe3cd1c110e600583c01ab7
onemen <tabmix.onemen@gmail.com>
2026-06-04 15:58:14 +0300

    clean log

--- test/timeout.test.ts
+++ test/timeout.test.ts
@@ -5,8 +5,13 @@ import stubVersions from './helpers/stubVersions'
 
 describe('timeout', async () => {
   let pkgPath: string
-  beforeAll(async () => {
+  let stub: { mockRestore: () => void }
+  beforeEach(async () => {
     pkgPath = await sandbox.createPackageJson({ dependencies: { express: '1' } })
+    stub = stubVersions({ express: '1' })
+  })
+  afterEach(async () => {
+    stub.mockRestore()
   })
 
   it('throw an exception instead of printing to the console when timeout is exceeded', async () => {
@@ -17,19 +22,16 @@ describe('timeout', async () => {
   })
 
   it('exit with error when timeout is exceeded', async () => {
-    const stub = stubVersions({ express: '1' })
-    try {
-      await runNcuCli(['--timeout', '1'], {
-        stdin: '{ "dependencies": { "express": "1" } }',
-      }).should.eventually.be.rejectedWith(/Exceeded global timeout of 1ms|Idle timeout reached/)
-    } finally {
-      stub.mockRestore()
-    }
+    await runNcuCli(['--timeout', '1', '--loglevel', 'silent'], {
+      stdin: '{ "dependencies": { "express": "1" } }',
+    }).should.eventually.be.rejectedWith(/Exceeded global timeout of 1ms|Idle timeout reached/)
   })
 
   it('completes successfully with timeout', async () => {
     const stub = stubVersions('99.9.9', { spawn: true })
-    await runNcuCli(['--timeout', '100000'], { stdin: '{ "dependencies": { "express": "1" } }' })
+    await runNcuCli(['--timeout', '100000', '--loglevel', 'silent'], {
+      stdin: '{ "dependencies": { "express": "1" } }',
+    })
     stub.mockRestore()
   })
 })
